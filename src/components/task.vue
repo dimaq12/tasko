5de71dbf10a0aeabca52cecc05dd982c66e4e5ff
dimaq12 <dimaq12@yandex.ru>
2017-05-26 16:37:14 +0300

    sort logic for tasks

--- src/components/task.vue
+++ src/components/task.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="task-list">
-    <div v-for="task in getTasks">
-      {{ task.title }}, {{task.project}}
+    
+    <div v-for="date in uniqDates">
+      {{ date }}
+        <div v-for="task in getTasks">
+          {{ task.title }}, {{task.project}}
+        </div>
     </div>
   </div>
 </template>
@@ -9,15 +13,30 @@
 export default{
   data: function(){
     return {
-      taskList: '',
+      taskList: [],
       uniqDates: []
+    }
+  },
+  methods: {
+    getUniqDates: function(list){
+      let uniqDatesArray = [];
+      for (let el in list){
+        console.log(list.el)
+        if (uniqDatesArray.indexOf(moment(list[el].start).format("YYYY-MM-DD")) == -1){
+          uniqDatesArray.push(moment(list[el].start).format("YYYY-MM-DD"));
+        }
+      }
+      console.log(uniqDatesArray)
+      return uniqDatesArray;
     }
   },
   computed: {
     getTasks: function(data){
       this.$http.get('')
         .then(response => this.taskList = response.body, response => console.log('error', response));
-        return this.taskList
+        this.uniqDates = this.getUniqDates(this.taskList);
+
+        return this.taskList;
     }
   }
 }
